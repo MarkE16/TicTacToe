b65220973e0ba7874671dfa421806ab56cd6c91f
Mark <markevola24@gmail.com>
2023-05-28 14:30:59 -0500

    finished creating ai and improved code

--- src/Main.java
+++ src/Main.java
@@ -8,38 +8,19 @@ public class Main {
   public static void startGame(boolean playAI) {
     TicTacToe game = new TicTacToe(playAI);
     Scanner input = new Scanner(System.in);
-    boolean playing = true;
 
-    while (playing) {
+    while (game.isPlaying()) {
+      if (game.AIModeActive() && game.getTurns() != 0 && game.isPlaying()) {
+        game.runAITurn();
+      }
       game.printBoard();
 
       System.out.println("Player " + game.getCurrentPlayer() + ", your turn:");
-      System.out.print("Row: ");
-      int row = input.nextInt();
-      System.out.print("Column: ");
-      int col = input.nextInt();
-
-      input.nextLine(); // Make inputs work again
+      int row = game.getRow();
+      int col = game.getCol();
 
       if (game.isValidPosition(row, col)) {
         game.play(row, col);
-        if (game.hasWinner()) {
-          System.out.println("----- Final Board -----");
-          game.printBoard();
-          System.out.print("Congratulations! Player " + game.getCurrentPlayer() + " wins! Play Again? (y/n): ");
-          char response = input.nextLine().charAt(0);
-          if (response == 'n') {
-            System.out.println("Goodbye!");
-            playing = false;
-            return;
-          }
-          game.restart();
-        } else {
-          game.incrementTurn();
-        }
-        if (game.AIModeActive()) {
-          game.runAITurn();
-        }
       } else {
         System.out.println("[!] Position invalid. Select a different one.");
       }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToe {
   private int turns = 0;
   private final char[][] board = new char[3][3];
   private boolean AIMode;
+  private Scanner input;
+  private boolean playing;
 
   private void createBoard() {
     for (char[] chars : board) {
@@ -11,12 +14,32 @@ public class TicTacToe {
     }
   }
 
+  public boolean isPlaying() {
+    return this.playing;
+  }
+
   public TicTacToe(boolean AIMode) {
     if (AIMode) {
       System.out.println("[!] Playing in AI mode.");
     }
     this.AIMode = AIMode;
+    this.input = new Scanner(System.in);
+    this.playing = true;
     this.createBoard(); // When creating the object, initialize the board with default values.
+  }
+
+  public int getRow() {
+    System.out.print("Row: ");
+    int row = input.nextInt();
+    input.nextLine();
+    return row;
+  }
+
+  public int getCol() {
+    System.out.print("Col: ");
+    int col = input.nextInt();
+    input.nextLine();
+    return col;
   }
 
   public void printBoard() {
@@ -51,6 +74,15 @@ public class TicTacToe {
 
   public void play(int row, int col) {
     this.board[row][col] = this.getCurrentPlayer();
+
+    if (this.hasWinner()) {
+      handleGameOver(true);
+    } else if (!this.hasWinner() && this.getNumOfPositions() == 0) {
+      handleGameOver(false);
+    }
+    else {
+      this.incrementTurn();
+    }
   }
 
   public boolean AIModeActive() {
@@ -58,19 +90,8 @@ public class TicTacToe {
   }
 
   public void runAITurn() {
-    int emptySpots = 0;
-    int[][] spotCords;
-
-    // Get the number of empty positions.
-    for (int row = 0; row < this.board.length; row++) {
-      for (int col = 0; col < this.board[row].length; col++) {
-        if (this.isValidPosition(row, col)) {
-          emptySpots++;
-        }
-      }
-    }
-
-    spotCords = new int[emptySpots][2];
+    int emptySpots = this.getNumOfPositions();
+    int[][] spotCords = new int[emptySpots][2];
     int arrIdx = 0;
 
     // Get all the current empty positions.
@@ -89,11 +110,6 @@ public class TicTacToe {
       spotCords[random][0], // The chosen row
       spotCords[random][1] // The chosen col
     );
-    if (this.hasWinner()) {
-      System.out.println("GAME OVER! THE AI WON!");
-    } else {
-      this.incrementTurn();
-    }
   }
 
   private boolean horizontalWin(char player) {
@@ -136,19 +152,55 @@ public class TicTacToe {
     );
   }
 
-  public boolean hasWinner() {
+  private boolean hasWinner() {
     char player = this.getCurrentPlayer();
     return this.horizontalWin(player) || this.verticalWin(player) || this.diagonalWin(player);
   }
 
-  public void restart() {
+  private void restart() {
     this.createBoard();
     this.turns = 0;
   }
 
-  public void incrementTurn() {
+  private void incrementTurn() {
     this.turns++;
   }
+
+  public int getTurns() {
+    return this.turns;
+  }
+
+  private int getNumOfPositions() {
+    int total = 0;
+    for (int row = 0; row < this.board.length; row++) {
+      for (int col = 0; col < this.board[row].length; col++) {
+        if (this.isValidPosition(row, col)) {
+          total++;
+        }
+      }
+    }
+
+    return total;
+  }
+
+  private void handleGameOver(boolean isWin) {
+    System.out.println("----- Final Board -----");
+    this.printBoard();
+    System.out.println();
+    if (isWin) {
+      System.out.print("Congratulations! Player " + this.getCurrentPlayer() + " wins! Play Again? (y/n): ");
+    } else {
+      System.out.print("GAME OVER! It's a tie! Play Again? (y/n): ");
+    }
+    char response = input.nextLine().charAt(0);
+    if (response == 'n') {
+      System.out.println("Goodbye!");
+      this.playing = false;
+      return;
+    }
+    this.restart();
+  }
+
 
   @Override
   public String toString() {
